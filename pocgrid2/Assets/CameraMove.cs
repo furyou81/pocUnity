c1f95ae18e77afe7d5d2a4bc5600c13d4cb22589
Leo-taro FUJIMOTO <lfujimot@e1r1p14.42.fr>
2019-08-14 17:10:26 +0200

    floor and camera

---
+++ pocgrid2/Assets/CameraMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+	public float panSpeed = 20f;
+	
+	void Update () {
+		Vector3 cameraPosition = transform.position;
+
+		if (Input.GetKey("w")) {
+			cameraPosition.z += panSpeed * Time.deltaTime;
+		}	
+	}
+}
